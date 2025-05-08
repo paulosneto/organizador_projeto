135835a4c1ffb3eb6e6cb280da6c3b02a3f8e712
paulosneto <paulo_sneto@yahoo.com>
2025-05-08 13:42:46 -0300

    fix: alter annotation Advice

--- src/main/java/com/project/organizer/exceptions/ExceptionHandlerCustom.java
+++ src/main/java/com/project/organizer/exceptions/ExceptionHandlerCustom.java
@@ -4,11 +4,12 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ExceptionHandlerCustom  extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ExceptionCustomError.class)
