ed2a1ada3552b5b1bde1548443a30e2df687cc59
paulosneto <paulo_sneto@yahoo.com>
2025-05-08 01:31:49 -0300

    fix: adding method in repository

--- src/main/java/com/project/organizer/repositories/TarefasRepository.java
+++ src/main/java/com/project/organizer/repositories/TarefasRepository.java
@@ -3,5 +3,8 @@ package com.project.organizer.repositories;
 import com.project.organizer.models.Tarefas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TarefasRepository extends JpaRepository<Tarefas, Integer> {
+    List<Tarefas> findTarefaByDescricaoTarefaContainingIgnoreCase(String valor);
 }
