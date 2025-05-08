c6e7e3e04429348a9f90176538a889d9c33868f0
paulosneto <paulo_sneto@yahoo.com>
2025-05-08 01:30:57 -0300

    fix: remove annotations

--- src/main/java/com/project/organizer/dto/TarefasDTO.java
+++ src/main/java/com/project/organizer/dto/TarefasDTO.java
@@ -1,14 +1,19 @@
 package com.project.organizer.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record TarefasDTO(@NotNull @JsonProperty String descricaoTarefa,
-                         @JsonProperty String projeto,
-                         @JsonProperty LocalDateTime dataInicio,
-                         @JsonProperty LocalDateTime dataFim,
-                         @JsonProperty String tarefaPredecedora,
-                         @JsonProperty String statusTarefa) {
+public record TarefasDTO(@NotBlank(message = "Descição é obrigatória") String descricaoTarefa,
+                          Integer idprojeto,
+                          LocalDate dataInicio,
+                          LocalDate dataFim,
+                          Integer idTarefaPredecedora,
+                          String statusTarefa) {
 }
